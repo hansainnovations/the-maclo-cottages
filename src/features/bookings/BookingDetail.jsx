df3e6e78fa54e30597ead5e80c329d80795f064e
PARMINDER  PATIAL <49855829+parmpatial@users.noreply.github.com>
2025-01-12 22:37:29 +0530

    Intial 4d time

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -17,7 +17,7 @@ import { useCheckout } from "../check-in-out/useCheckout";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import { useDeleteBooking } from "./useDeleteBooking";
-import Spinner from "../../ui/spinner";
+import Spinner from "../../ui/Spinner";
 import Empty from "../../ui/Empty";
 
 const HeadingGroup = styled.div`
